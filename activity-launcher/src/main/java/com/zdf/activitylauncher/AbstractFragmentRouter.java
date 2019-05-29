aeb1a0f1630e643b4d954e186d7d99a00214e238
Carlos2927 <yonggong615@gmail.com>
2019-05-29 16:43:48 +0800

    删除原项目的actvity-launcher,将其代码放到activity_launcher

--- activity-launcher/src/main/java/com/zdf/activitylauncher/AbstractFragmentRouter.java
+++
@@ -1,7 +0,0 @@
-package com.zdf.activitylauncher;
-
-import android.content.Intent;
-
-public interface AbstractFragmentRouter {
-    void startActivityForResult(Intent intent, ActivityLauncher.Callback callback);
-}
